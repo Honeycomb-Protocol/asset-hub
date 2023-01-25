6485f7c66109a84230c49f73910a9105c9522a0a
Ghulam Mustafa Raza Qadri <ghulammustafasoomar@gmail.com>
2023-01-25 17:46:40 +0500

    Changes

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,9 +1,20 @@
-const fs = require('fs');
-const { spawnSync } = require("child_process")
+const fs = require("fs");
+const { spawnSync } = require("child_process");
 
-if (!fs.existsSync('./node_modules')) spawnSync("yarn", ["install"], { stdio: 'inherit' });
-if (!fs.existsSync('./node_modules/@metaplex-foundation/mpl-candy-guard/node_modules')) {
-    spawnSync("yarn", ["install"], { stdio: 'inherit', cwd: "./node_modules/@metaplex-foundation/mpl-candy-guard/" });
-    spawnSync("yarn", ["run build"], { stdio: 'inherit', cwd: "./node_modules/@metaplex-foundation/mpl-candy-guard/" });
+if (!fs.existsSync("./node_modules"))
+  spawnSync("yarn", ["install"], { stdio: "inherit" });
+if (
+  fs.existsSync("../@metaplex-foundation/mpl-candy-guard") &&
+  !fs.existsSync("../@metaplex-foundation/mpl-candy-guard/dist")
+) {
+  if (!fs.existsSync("../@metaplex-foundation/mpl-candy-guard/node_modules"))
+    spawnSync("yarn", ["install"], {
+      stdio: "inherit",
+      cwd: "../@metaplex-foundation/mpl-candy-guard/",
+    });
+  spawnSync("yarn", ["run build"], {
+    stdio: "inherit",
+    cwd: "../@metaplex-foundation/mpl-candy-guard/",
+  });
 }
-spawnSync("yarn", ["run build"], { stdio: 'inherit' });
+spawnSync("yarn", ["run build"], { stdio: "inherit" });
