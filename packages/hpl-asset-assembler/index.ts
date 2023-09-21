c8f9e9fade958255b61a41f31429a6c6f178c65e
sultandilaram <sultanndilaram@gmail.com>
2023-09-21 22:46:05 +0500

    PROGRAM_IDs explicit expoorts

--- packages/hpl-asset-assembler/index.ts
+++ packages/hpl-asset-assembler/index.ts
@@ -3,3 +3,5 @@ export * from "./operations";
 export * from "./types";
 export * from "./pdas";
 export * from "./AssetAssembler";
+
+export { PROGRAM_ID as HPL_ASSET_ASSEMBLER_PROGRAM } from "./generated";
--- packages/hpl-asset-manager/index.ts
+++ packages/hpl-asset-manager/index.ts
@@ -2,3 +2,5 @@ export * from "./generated";
 export * from "./operations";
 export * from "./pdas";
 export * from "./AssetManager";
+
+export { PROGRAM_ID as HPL_ASSET_MANAGER_PROGRAM } from "./generated";
