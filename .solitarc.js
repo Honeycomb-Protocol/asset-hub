2a23b71bb9fcfada934fa771a6fd1179e579a6bc
sultandilaram <sultanndilaram@gmail.com>
2023-10-26 19:46:23 +0500

    remvoed comment

--- .solitarc.js
+++ .solitarc.js
@@ -62,10 +62,10 @@ const createConfig = (name, programId) => {
     programDir: path.join(__dirname, "programs", packageName),
     removeExistingIdl: false,
     idlHook: (idl) => {
-      // idl = {
-      //   ...idl,
-      //   ...require("./target/idl/" + programName + ".json"),
-      // };
+      idl = {
+        ...idl,
+        ...require("./target/idl/" + programName + ".json"),
+      };
       const variantsOfConditinal = {};
       const okTypes = (type) => {
         if (type.defined.startsWith("Conditional<")) {
