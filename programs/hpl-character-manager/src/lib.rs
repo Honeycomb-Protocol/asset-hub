d702af783fd091def09e987450d3e9be823ccb81
Sultan Nadeem <sultanndilaram@gmail.com>
2024-01-08 20:05:33 +0500

    program finalizations

--- programs/hpl-character-manager/src/lib.rs
+++ programs/hpl-character-manager/src/lib.rs
@@ -22,4 +22,44 @@ pub mod hpl_character_manager {
     ) -> Result<()> {
         instructions::new_character_model(ctx, args)
     }
+
+    pub fn deposit_nft(ctx: Context<DepositNft>) -> Result<()> {
+        instructions::deposit_nft(ctx)
+    }
+
+    pub fn withdraw_nft(ctx: Context<WithdrawNft>) -> Result<()> {
+        instructions::withdraw_nft(ctx)
+    }
+
+    pub fn deposit_cnft<'info>(
+        ctx: Context<'_, '_, '_, 'info, DepositCnft<'info>>,
+        args: DepositCnftArgs,
+    ) -> Result<()> {
+        instructions::deposit_cnft(ctx, args)
+    }
+
+    pub fn withdraw_cnft<'info>(
+        ctx: Context<'_, '_, '_, 'info, WithdrawCnft<'info>>,
+        args: WithdrawCnftArgs,
+    ) -> Result<()> {
+        instructions::withdraw_cnft(ctx, args)
+    }
+
+    pub fn wrap_character(ctx: Context<WrapCharacter>) -> Result<()> {
+        instructions::wrap_character(ctx)
+    }
+
+    pub fn unwrap_character<'info>(
+        ctx: Context<'_, '_, '_, 'info, UnwrapCharacter<'info>>,
+        args: UnwrapCharacterArgs,
+    ) -> Result<()> {
+        instructions::unwrap_character(ctx, args)
+    }
+
+    pub fn use_character<'info>(
+        ctx: Context<'_, '_, '_, 'info, UseCharacter<'info>>,
+        args: UseCharacterArgs,
+    ) -> Result<()> {
+        instructions::use_character(ctx, args)
+    }
 }
