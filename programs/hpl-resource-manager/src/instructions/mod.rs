85802af1323965f88cd6aec8c1e7e637eb87a971
Shayan M Hussain <shayanhussain48@gmail.com>
2024-02-01 19:44:50 +0500

    misc

--- programs/hpl-resource-manager/src/instructions/mod.rs
+++ programs/hpl-resource-manager/src/instructions/mod.rs
@@ -1,3 +1,4 @@
+pub mod holding;
 pub mod resource;
 
-pub use resource::*;
+pub use {holding::*, resource::*};
--- programs/hpl-resource-manager/src/lib.rs
+++ programs/hpl-resource-manager/src/lib.rs
@@ -13,10 +13,28 @@ declare_id!("6ARwjKsMY2P3eLEWhdoU5czNezw3Qg6jEfbmLTVQqrPQ");
 pub mod hpl_resource_manager {
     use super::*;
 
-    pub fn create_new_resource(
-        ctx: Context<CreateNewResource>,
-        args: CreateNewResourceArgs,
+    pub fn create_resource(ctx: Context<CreateResource>, args: CreateResourceArgs) -> Result<()> {
+        instructions::create_resource(ctx, args)
+    }
+
+    pub fn initilize_resource_tree(
+        ctx: Context<InitilizeResourceTree>,
+        args: InitilizeResourceTreeArgs,
     ) -> Result<()> {
-        instructions::create_new_resource(ctx, args)
+        instructions::initilize_resource_tree(ctx, args)
+    }
+
+    pub fn mint_resource<'info>(
+        ctx: Context<'_, '_, '_, 'info, MintResource<'info>>,
+        args: MintResourceArgs,
+    ) -> Result<()> {
+        instructions::mint_resource(ctx, args)
+    }
+
+    pub fn burn_resource<'info>(
+        ctx: Context<'_, '_, '_, 'info, BurnResource<'info>>,
+        args: BurnResourceArgs,
+    ) -> Result<()> {
+        instructions::burn_resource(ctx, args)
     }
 }
