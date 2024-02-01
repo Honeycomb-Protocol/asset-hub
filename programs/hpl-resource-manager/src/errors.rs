5bbeefb845b29f4f5ed8798f438c771e96eea040
Shayan M Hussain <shayanhussain48@gmail.com>
2024-02-01 19:42:51 +0500

    added some InsufficientAmount error

--- programs/hpl-resource-manager/src/errors.rs
+++ programs/hpl-resource-manager/src/errors.rs
@@ -4,4 +4,7 @@ use anchor_lang::prelude::*;
 pub enum ResourceErrorCode {
     #[msg("The resource is not initialized")]
     ResourceNotInitialized,
+
+    #[msg("The amount provided for burning is greater than the amount of the held in the account")]
+    InsufficientAmount,
 }
