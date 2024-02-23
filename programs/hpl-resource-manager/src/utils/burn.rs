5c0b86633ed877191ab998b702de5639d6563ff7
Shayan M Hussain <shayanhussain48@gmail.com>
2024-02-23 23:21:07 +0500

    spit the burn & mint functions with respect to fung & non tokens

--- programs/hpl-resource-manager/src/utils/burn.rs
+++ programs/hpl-resource-manager/src/utils/burn.rs
@@ -1,7 +1,10 @@
 use {
-    crate::{errors::ResourceErrorCode, Holding, HoldingAccountArgs, Resource},
+    crate::{errors::ResourceErrorCode, BurnResourceArgs, Holding, HoldingAccountArgs, Resource},
     anchor_lang::prelude::*,
-    hpl_toolkit::compression::{CompressedData, CompressedDataEvent, ToNode},
+    hpl_toolkit::{
+        compression::{CompressedDataEvent, ToNode},
+        CompressedData,
+    },
     spl_account_compression::{program::SplAccountCompression, Noop},
 };
 
@@ -12,60 +15,151 @@ pub fn use_burn_resource<'info>(
     clock: &Sysvar<'info, Clock>,
     log_wrapper: &Program<'info, Noop>,
     compression_program: &Program<'info, SplAccountCompression>,
-    holding_state: HoldingAccountArgs,
+    args: &BurnResourceArgs,
+) -> Result<()> {
+    match args {
+        BurnResourceArgs::Fungible {
+            holding_state,
+            amount,
+        } => {
+            burn_fungible_token(
+                resource,
+                merkle_tree,
+                remaining_accounts,
+                clock,
+                log_wrapper,
+                compression_program,
+                holding_state,
+                amount,
+            )?;
+        }
+
+        BurnResourceArgs::INF { holding_state } => {
+            burn_non_fungible_token(
+                resource,
+                merkle_tree,
+                remaining_accounts,
+                clock,
+                log_wrapper,
+                compression_program,
+                holding_state,
+            )?;
+        }
+    };
+
+    Ok(())
+}
+
+pub fn burn_fungible_token<'info>(
+    resource: &mut Account<'info, Resource>,
+    merkle_tree: &AccountInfo<'info>,
+    remaining_accounts: &[AccountInfo<'info>],
+    clock: &Sysvar<'info, Clock>,
+    log_wrapper: &Program<'info, Noop>,
+    compression_program: &Program<'info, SplAccountCompression>,
+    holding_state: &HoldingAccountArgs,
     amount: &u64,
 ) -> Result<()> {
-    msg!("checking for InsufficientAmount");
-    if *amount > holding_state.holding.balance {
-        msg!(
-            "InsufficientAmount {:?} {:?}",
-            amount,
-            holding_state.holding.balance
-        );
-        return Err(ResourceErrorCode::InsufficientAmount.into());
-    }
-
     let (_leaf_idx, seq) = resource
         .merkle_trees
         .assert_append(merkle_tree.to_owned())?;
 
-    let new_holding_state = Holding {
-        holder: holding_state.holding.holder,
-        balance: holding_state.holding.balance - amount,
-    };
+    if let Holding::Fungible { holder, balance } = holding_state.holding {
+        if *amount > balance {
+            msg!("InsufficientAmount {:?} {:?}", amount, balance);
+            return Err(ResourceErrorCode::InsufficientAmount.into());
+        }
 
-    let event = CompressedDataEvent::Leaf {
-        slot: clock.slot,
-        tree_id: merkle_tree.key().to_bytes(),
-        leaf_idx: holding_state.leaf_idx,
-        seq: seq,
-        stream_type: new_holding_state.event_stream(),
-    };
-    event.wrap(&log_wrapper)?;
+        let new_holding_state = Holding::Fungible {
+            holder: holder,
+            balance: balance - amount,
+        };
 
-    let new_leaf;
-    if holding_state.holding.balance - amount > 0 {
-        new_leaf = new_holding_state.to_compressed().to_node();
+        let event = CompressedDataEvent::Leaf {
+            slot: clock.slot,
+            tree_id: merkle_tree.key().to_bytes(),
+            leaf_idx: holding_state.leaf_idx,
+            seq: seq,
+            stream_type: new_holding_state.event_stream(),
+        };
+        event.wrap(&log_wrapper)?;
+
+        let new_leaf;
+        if balance - amount > 0 {
+            new_leaf = new_holding_state.to_compressed().to_node();
+        } else {
+            new_leaf = [0; 32];
+        }
+
+        let bump_binding = [resource.bump];
+        let signer_seeds = resource.seeds(&bump_binding);
+
+        // update the compressed token account using controlled merkle tree
+        hpl_toolkit::compression::replace_leaf(
+            holding_state.root,
+            holding_state.holding.to_compressed().to_node(),
+            new_leaf,
+            holding_state.leaf_idx,
+            &resource.to_account_info(),
+            &merkle_tree,
+            &compression_program,
+            &log_wrapper,
+            remaining_accounts.to_vec(),
+            Some(&[&signer_seeds[..]]),
+        )?;
     } else {
-        new_leaf = [0; 32];
+        return Err(ResourceErrorCode::InvalidHoldingState.into());
     }
 
-    let bump_binding = [resource.bump];
-    let signer_seeds = resource.seeds(&bump_binding);
+    Ok(())
+}
 
-    // update the compressed token account using controlled merkle tree
-    hpl_toolkit::compression::replace_leaf(
-        holding_state.root,
-        holding_state.holding.to_compressed().to_node(),
-        new_leaf,
-        holding_state.leaf_idx,
-        &resource.to_account_info(),
-        &merkle_tree,
-        &compression_program,
-        &log_wrapper,
-        remaining_accounts.to_vec(),
-        Some(&[&signer_seeds[..]]),
-    )?;
+pub fn burn_non_fungible_token<'info>(
+    resource: &mut Account<'info, Resource>,
+    merkle_tree: &AccountInfo<'info>,
+    remaining_accounts: &[AccountInfo<'info>],
+    clock: &Sysvar<'info, Clock>,
+    log_wrapper: &Program<'info, Noop>,
+    compression_program: &Program<'info, SplAccountCompression>,
+    holding_state: &HoldingAccountArgs,
+) -> Result<()> {
+    let (_leaf_idx, seq) = resource
+        .merkle_trees
+        .assert_append(merkle_tree.to_owned())?;
+
+    if let Holding::INF {
+        holder: _,
+        characteristics: _,
+    } = holding_state.holding
+    {
+        let event = CompressedDataEvent::Leaf {
+            slot: clock.slot,
+            tree_id: merkle_tree.key().to_bytes(),
+            leaf_idx: holding_state.leaf_idx,
+            seq: seq,
+            stream_type: holding_state.holding.event_stream(),
+        };
+        event.wrap(&log_wrapper)?;
+
+        let bump_binding = [resource.bump];
+        let signer_seeds = resource.seeds(&bump_binding);
+
+        // update the compressed token account using controlled merkle tree
+        hpl_toolkit::compression::replace_leaf(
+            holding_state.root,
+            holding_state.holding.to_compressed().to_node(),
+            [0; 32],
+            holding_state.leaf_idx,
+            &resource.to_account_info(),
+            &merkle_tree,
+            &compression_program,
+            &log_wrapper,
+            remaining_accounts.to_vec(),
+            Some(&[&signer_seeds[..]]),
+        )?;
+    } else {
+        return Err(ResourceErrorCode::InvalidHoldingState.into());
+    }
 
     Ok(())
 }
--- programs/hpl-resource-manager/src/utils/mint.rs
+++ programs/hpl-resource-manager/src/utils/mint.rs
@@ -1,7 +1,13 @@
 use {
-    crate::{Holding, MintResourceArgs, Resource},
+    crate::{
+        errors::ResourceErrorCode, Holding, HoldingAccountArgs, MintResourceArgs,
+        NonFungibleHolding, Resource,
+    },
     anchor_lang::prelude::*,
-    hpl_toolkit::compression::{CompressedData, CompressedDataEvent, ToNode},
+    hpl_toolkit::{
+        compression::{CompressedData, CompressedDataEvent, ToNode},
+        HashMap,
+    },
     spl_account_compression::{program::SplAccountCompression, Noop},
 };
 
@@ -17,17 +23,122 @@ pub fn use_mint_resource<'info>(
 ) -> Result<()> {
     msg!("verifying leaf");
 
+    match args {
+        MintResourceArgs::Fungible {
+            holding_state,
+            amount,
+        } => {
+            mint_fungible_resource(
+                resource,
+                merkle_tree,
+                owner,
+                remaining_accounts,
+                clock,
+                log_wrapper,
+                compression_program,
+                holding_state.to_owned(),
+                amount,
+            )?;
+            msg!("Minted {:?} Resources", amount);
+        }
+
+        MintResourceArgs::INF {
+            holding_state: _,
+            characteristics,
+        } => {
+            mint_non_fungible_resource(
+                resource,
+                merkle_tree,
+                owner,
+                clock,
+                log_wrapper,
+                compression_program,
+                characteristics.to_owned(),
+            )?;
+
+            msg!("Minted Non Fungible Resource");
+        }
+    }
+
+    Ok(())
+}
+
+pub fn mint_non_fungible_resource<'info>(
+    resource: &mut Account<'info, Resource>,
+    merkle_tree: &AccountInfo<'info>,
+    owner: &AccountInfo<'info>,
+    clock: &Sysvar<'info, Clock>,
+    log_wrapper: &Program<'info, Noop>,
+    compression_program: &Program<'info, SplAccountCompression>,
+    characteristics: HashMap<String, String>,
+) -> Result<()> {
     let (leaf_idx, seq) = resource
         .merkle_trees
         .assert_append(merkle_tree.to_account_info())?;
 
-    if let Some(holding_state) = &args.holding_state {
+    msg!("Minting Non Fungible Resource");
+    let holding_account = Holding::INF {
+        holder: NonFungibleHolding::Holder(owner.key()),
+        characteristics: characteristics.into_iter().collect(),
+    };
+
+    msg!("Event Stream ");
+    // event
+    let event = CompressedDataEvent::Leaf {
+        slot: clock.slot,
+        tree_id: merkle_tree.key().to_bytes(),
+        leaf_idx,
+        seq,
+        stream_type: holding_account.event_stream(),
+    };
+    event.wrap(&log_wrapper)?;
+
+    msg!("Compressing the holding account");
+    // create the compressed token account using controlled merkle tree
+    let compressed_holding = holding_account.to_compressed();
+    let bump_binding = [resource.bump];
+    let signer_seeds = resource.seeds(&bump_binding);
+
+    hpl_toolkit::compression::append_leaf(
+        compressed_holding.to_node(),
+        &resource.to_account_info(),
+        &merkle_tree,
+        &compression_program,
+        &log_wrapper,
+        Some(&[&signer_seeds[..]]),
+    )?;
+
+    Ok(())
+}
+
+pub fn mint_fungible_resource<'info>(
+    resource: &mut Account<'info, Resource>,
+    merkle_tree: &AccountInfo<'info>,
+    owner: &AccountInfo<'info>,
+    remaining_accounts: &[AccountInfo<'info>],
+    clock: &Sysvar<'info, Clock>,
+    log_wrapper: &Program<'info, Noop>,
+    compression_program: &Program<'info, SplAccountCompression>,
+    holding_state: Option<HoldingAccountArgs>,
+    amount: &u64,
+) -> Result<()> {
+    let (leaf_idx, seq) = resource
+        .merkle_trees
+        .assert_append(merkle_tree.to_account_info())?;
+
+    if let Some(holding_state) = &holding_state {
         let bump_binding = [resource.bump];
         let signer_seeds = resource.seeds(&bump_binding);
-        let new_holding_state = Holding {
-            holder: holding_state.holding.holder,
-            balance: holding_state.holding.balance + args.amount,
-        };
+
+        let new_holding_state;
+        if let Holding::Fungible { holder, balance } = holding_state.holding {
+            new_holding_state = Holding::Fungible {
+                holder: holder,
+                balance: balance + amount,
+            };
+        } else {
+            return Err(ResourceErrorCode::InvalidHoldingState.into());
+        }
 
         let event = CompressedDataEvent::Leaf {
             slot: clock.slot,
@@ -52,10 +163,10 @@ pub fn use_mint_resource<'info>(
             Some(&[&signer_seeds[..]]),
         )?;
     } else {
-        msg!("Minting without default holding state");
-        let holding_account = Holding {
-            holder: owner.key(),
-            balance: args.amount,
+        msg!("Minting without the holding state");
+        let holding_account = Holding::Fungible {
+            holder: owner.key.to_owned(),
+            balance: *amount,
         };
 
         msg!("Event Stream ");
@@ -83,9 +194,7 @@ pub fn use_mint_resource<'info>(
             &log_wrapper,
             Some(&[&signer_seeds[..]]),
         )?;
-    }
-
-    msg!("Minted {:?} Resources", args.amount);
+    };
 
     Ok(())
 }
